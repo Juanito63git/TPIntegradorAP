90f9dd667c0acc63015291bab8767fdb52ffb39b
Juan IBARRA <juanibarra63@outlook.com>
2023-04-30 12:41:47 -0300

    Agregamos proyectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 // Import ng-circle-progress
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardsoftskillsComponent } from './components/hardsoftskills/hardsoftskills.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HardsoftskillsComponent } from './components/hardsoftskills/hardsoftski
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HardsoftskillsComponent
+    HardsoftskillsComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
